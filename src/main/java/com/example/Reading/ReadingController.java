5dee37926843bbf447d1e74c4620b0ac2d6a9743
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-16 17:35:51 -0400

    hello controller

---
+++ src/main/java/com/example/Reading/ReadingController.java
@@ -0,0 +1,12 @@
+package com.example.Reading;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReadingController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
