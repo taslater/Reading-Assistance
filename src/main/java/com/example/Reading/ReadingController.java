90291899daf84c6ea6de92735a124bec94c4a2fd
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-16 20:41:08 -0400

    re-added request mapping

--- src/main/java/com/example/Reading/ReadingController.java
+++ src/main/java/com/example/Reading/ReadingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ReadingController {
     @GetMapping("/hello")
     public String hello() {
