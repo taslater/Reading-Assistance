71872a390f771d36fb8294bbbe0ba2655b439e90
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-16 20:13:46 -0400

    removed request mapping

--- src/main/java/com/example/Reading/ReadingController.java
+++ src/main/java/com/example/Reading/ReadingController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class ReadingController {
     @GetMapping("/hello")
     public String hello() {
