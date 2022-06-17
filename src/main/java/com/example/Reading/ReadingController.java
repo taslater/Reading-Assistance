77cce3005aecbc44d68fa6f797ac502843d6b31f
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-16 20:44:42 -0400

    changed test string again

--- src/main/java/com/example/Reading/ReadingController.java
+++ src/main/java/com/example/Reading/ReadingController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReadingController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello again, friend!";
+        return "Hello again... again, friend!";
     }
 }
